1b4665ff1ffafca8cdcfbafb0a13c96ed18b2f0c
Ray Turner <rturner@basecampcodingacademy.org>
2019-04-25 13:12:45 -0500

    more array cardio

---
+++ 25_April/array_cardio.py
@@ -0,0 +1,20 @@
+def find_pair(socks):
+    matches = []
+    singles = []
+    pairs = 0
+
+    for sock in socks:
+        if sock not in singles:
+            singles.append(sock)
+
+        elif sock in singles:
+            pairs += 1
+            singles.remove(sock)
+    print(pairs)
+
+
+arr1 = ['blue', 'blue', 'yellow', 'green', 'orange', 'purple', 'red', 'black']
+arr2 = ['blue', 'purple', 'yellow', 'blue', 'orange', 'purple', 'red', 'black']
+
+# find_pair(arr1)
+find_pair(arr2)
